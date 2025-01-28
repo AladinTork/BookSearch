072519ee64eba2e5bc645145f0d3884f4e75c0dd
Arman Tork <canadienperse@gmail.com>
2025-01-28 18:13:02 -0500

    fixed the login

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,8 +3,8 @@ import { gql } from '@apollo/client';
 
 // Mutation to log in a user
 export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
-    loginUser(email: $email, password: $password) {
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
       user {
         _id
