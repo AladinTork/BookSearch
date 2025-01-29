9939c754c0e69d8ab95a6dd78eba4ee8966e3b95
Arman Tork <canadienperse@gmail.com>
2025-01-28 22:01:28 -0500

    dist

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ async function startServer() {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
   db.once('open', () => {
